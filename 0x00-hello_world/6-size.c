3a0848f6904f954d94674b3ab2d9ea72f07c8ff1
St-Mavic <manyozenith@gmail.com>
2023-07-13 19:30:59 +0000

    size of datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-printf("Size of a char: %ul byte(s)", sizeof(char));
-printf("Size of a int: %ul byte(s)", sizeof(int));
-printf("Size of a long int: %ul byte(s)", sizeof(long int));
-printf("Size of a long long int: %ul byte(s)", sizeof(long long int));
-printf("Size of a float: %ul byte(s)", sizeof(float));
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("Size of a int: %lu byte(s)", sizeof(int));
+printf("Size of a long int: %lu byte(s)", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("Size of a float: %lu byte(s)", sizeof(float));
 
 return (0);
 }
