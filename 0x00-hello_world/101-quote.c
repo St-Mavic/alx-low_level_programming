c8cf13062fc591b047a634eaaaf295e5f09bccbc
St-Mavic <manyozenith@gmail.com>
2023-07-13 20:33:24 +0000

    qoute output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry poin
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+return (0);
+}
