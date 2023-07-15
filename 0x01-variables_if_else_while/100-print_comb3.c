d550253fded2010f01a7536a499843ed09a8084b
St-Mavic <manyozenith@gmail.com>
2023-07-15 05:06:41 +0000

    printing combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+int a;
+int b;
+
+for (a = 0; a < 10; a++)
+{
+for (b = a + 1; b < 10; b++)
+{
+putchar(a + '0');
+putchar(b + '0');
+if (a != 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
