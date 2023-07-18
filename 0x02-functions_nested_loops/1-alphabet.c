6a98714fb9e1d7c5cab0d58c5d5e208ecac70b33
St-Mavic <manyozenith@gmail.com>
2023-07-18 23:52:13 +0000

    printing lower case characters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar (a);
+	}
+	_putchar('\n');
+}
