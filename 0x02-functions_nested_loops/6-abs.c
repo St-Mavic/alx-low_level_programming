e35b3b341eed6fdfa89181d51e8de379f30f227a
St-Mavic <manyozenith@gmail.com>
2023-07-19 01:40:25 +0000

    printing absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value
+ * @a: checks for integer
+ * Return: always 0
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (-a);
+	}
+	else
+		return (a);
+}
