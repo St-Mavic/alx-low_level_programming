ec79ad7384c8a31af99da086f78ebe13ffaff87b
St-Mavic <manyozenith@gmail.com>
2023-07-21 15:05:04 +0000

    printing squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - orinting squares
+ * @size: checks the parameter
+ * Return: no value
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
+
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
