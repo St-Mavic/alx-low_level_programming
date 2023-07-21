ceb7821cf7b714531aa9b6e62808a76528a2a337
St-Mavic <manyozenith@gmail.com>
2023-07-21 10:50:10 +0000

    printing diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - drawing diagonal lines
+ * @n: parameter variable
+ * Return: no value
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= n; a++)
+		{
+			for (b = 0; b <= a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+
+}
