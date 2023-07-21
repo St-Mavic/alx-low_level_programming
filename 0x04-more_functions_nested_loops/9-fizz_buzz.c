186b4bfe936cc69a09e4bca750d6556121b40f65
St-Mavic <manyozenith@gmail.com>
2023-07-21 15:47:19 +0000

    printing fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int g;
+
+	for (g = 1; g <= 100; g++)
+	{
+		if (g % 3 == 0 && g % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (g % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (g % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", g);
+	}
+	putchar('\n');
+	return (0);
+
+}
