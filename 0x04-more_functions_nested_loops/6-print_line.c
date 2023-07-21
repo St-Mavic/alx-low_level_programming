bdf265bc0ee8e2a6ad35044ef8b10aca08ed0ebc
St-Mavic <manyozenith@gmail.com>
2023-07-21 09:33:49 +0000

    printing straight lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - printing a straight line
+ * @n: accepts argument
+ * Return: no value
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+			_putchar('-');
+	}
+	_putchar('\n');
+}
