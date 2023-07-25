cd2804581ade347aeb19404571a314604155497b
St-Mavic <manyozenith@gmail.com>
2023-07-25 01:24:00 +0000

    copying strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strcpy - copy a string
  * @dest: destination
@@ -10,11 +10,15 @@
  
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	char *res = dest;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (res);
 
-	return (dest);
 }
