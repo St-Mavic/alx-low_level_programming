263b2379e899735b3d51695a5e2869fbab2b3a7c
St-Mavic <manyozenith@gmail.com>
2023-08-01 23:02:53 +0000

    searching a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - function
+ * @s: first string
+ * @accept: second string
+ *
+ * Return: null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
