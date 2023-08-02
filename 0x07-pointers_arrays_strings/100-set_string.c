094c827a4f65bc662e8d50cea8f428e4d7ffab54
St-Mavic <manyozenith@gmail.com>
2023-08-02 00:57:07 +0000

    pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - pointers to char
+ * @s: para
+ * @to: second parameter
+ *
+ * Return: no value
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
