48ac02eb9ea1addbf8a50b30d260a8d8e1aefe5d
St-Mavic <manyozenith@gmail.com>
2023-08-29 23:27:09 +0000

    a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,23 +1,24 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts
- * a new node at a given position.
- * @head: a pointer to head of list
- * idx: index of the list where the new node should be added
- * @n: integer to be added to the new node.
- * Return: address of the new element, or NULL if it failed
+ * insert_nodeint_at_index - a function that inserts a new
+ * node at a given position.
+ * @head: pointer to  head of list
+ * @idx: index element to be added
+ * @n: element to be added
+ *
+ * Return: address of new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *tmp_node;
-	unsigned int a;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *tmp_node;
+	unsigned int a = 0;
 
-	new_node = malloc(sizeof(listint_t));
-
-	if (head == NULL)
+	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
 
 	if (idx == 0)
 	{
@@ -27,18 +28,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	tmp_node = *head;
-	for (a = 0; tmp_node != NULL && a < idx -1; a++)
+
+	while (tmp_node)
+	{
+		if (a == idx - 1)
+		{
+			new_node->next = tmp_node->next;
+			tmp_node->next = new_node;
+			return (new_node);
+		}
 		tmp_node = tmp_node->next;
-
-	if (tmp_node == NULL)
-		return (NULL);
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = tmp_node->next;
-	tmp_node->next = new_node;
-
-	return (new_node);
+		a++;
+	}
+	free(new_node);
+	return (NULL);
 }
